2599b0814e7f182ec3cf1016817167a585888c93
arthurzx-ww <arthur.veiga1105@gmail.com>
2025-05-21 19:53:58 -0300

    correcao banner

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ const bars = document.getElementById("bars");
 const nav = document.getElementById("nav");
 const navItem = document.querySelectorAll("a");
 
-bars.addEventListener("click", toggleMenu);
 function toggleMenu() {
   console.log("clicou no menu");
   bars.classList.toggle("active");
   nav.classList.toggle("active");
 }
+
+bars.addEventListener("click", toggleMenu);
+
 navItem.forEach((item) => {
   item.addEventListener("click", toggleMenu);
 });
